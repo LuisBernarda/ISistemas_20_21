0c4ade656fa6092ea69f90f1cdeef8b2670dedd5
FranciscoSantosNeto <50747688+FranciscoSantosNeto@users.noreply.github.com>
2020-12-06 19:02:07 +0000

    Update Main.cs

--- Projeto_IS/Main.cs
+++ Projeto_IS/Main.cs
@@ -223,7 +223,7 @@ namespace Projeto_IS
             if (result == DialogResult.OK)
             {
                 string folderName = folderBrowserDialog1.SelectedPath;
-                MessageBox.Show(inPath);
+                //MessageBox.Show(inPath);
                 string path = $@"{folderName}\Output.html";
                 if (!File.Exists(path))
                 {
@@ -238,75 +238,11 @@ namespace Projeto_IS
             DataTable dtTable = new DataTable();
             dtTable = convertStringToDataTable(jsonString);
             htmlString = ExportDatatableToHtml(dtTable);
-            
-
-           
-    
 
             return htmlString;
         }
 
-        private String jsonToDatatable(String jsonString)
-        {
-
-            string output = "";
-
-            output = $"{jsonString}.txt";
-
-
-            DataTable dtTable = new DataTable();
-            List<string> rowList = new List<string>();
-            ISheet sheet;
-            using (var stream = new FileStream(jsonString, FileMode.Open))
-            {
-                stream.Position = 0;
-                XSSFWorkbook xssWorkbook = new XSSFWorkbook(stream);
-                sheet = xssWorkbook.GetSheetAt(0);
-                IRow headerRow = sheet.GetRow(0);
-                int cellCount = headerRow.LastCellNum;
-                for (int j = 0; j < cellCount; j++)
-                {
-                    ICell cell = headerRow.GetCell(j);
-                    if (cell == null || string.IsNullOrWhiteSpace(cell.ToString())) continue;
-                    {
-                        dtTable.Columns.Add(cell.ToString());
-                    }
-                }
-                for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum - 1; i++)
-                {
-                    IRow row = sheet.GetRow(i);
-                    if (row == null) continue;
-                    if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
-                    for (int j = row.FirstCellNum; j < cellCount; j++)
-                    {
-                        if (row.GetCell(j) != null)
-                        {
-                            if (!string.IsNullOrEmpty(row.GetCell(j).ToString()) && !string.IsNullOrWhiteSpace(row.GetCell(j).ToString()))
-                            {
-                                rowList.Add(row.GetCell(j).ToString());
-                            }
-                        }
-                    }
-                    if (rowList.Count > 0)
-                        dtTable.Rows.Add(rowList.ToArray());
-                    rowList.Clear();
-                }
-            }
-            jsonString = JsonConvert.SerializeObject(dtTable);
-            MessageBox.Show(jsonString);
-            using (FileStream fs = new FileStream(output, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Write))
-            {
-                StreamWriter write = new StreamWriter(fs);
-                write.Write(JsonConvert.SerializeObject(dtTable));
-                write.Flush();
-                write.Close();
-                fs.Close();
-            }
-
-            ExportDatatableToHtml(dtTable);
-
-            return jsonString;
-        }
+        
 
         public static DataTable convertStringToDataTable(string jsonString)
         {
